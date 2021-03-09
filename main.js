a14269afb6e0fa60adcc597b42fb066b337cecf7
bwbsungjicho <63273235+bwbsungjicho@users.noreply.github.com>
2021-03-09 22:13:56 +1100

    First
    Trying to host forever

---
+++ main.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '-';
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('pong!');
+    }
+    if (command === 'f') {
+        message.channel.send(message.author.username + " has paid their respect");
+    }
+});
+
+client.login(process.env.token);
