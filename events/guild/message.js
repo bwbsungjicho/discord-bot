39bf8d1cceca3f2eb1b0471544cd834a4270666a
Sung Ji Cho <Sung.Cho34@education.nsw.gov.au>
2021-06-20 22:46:38 +1000

    removed profileSchema, as that was only a test.

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,4 +1,3 @@
-const profileModel = require("../../models/profileSchema");
 const eventModel = require("../../models/eventSchema");
 
 module.exports = async (Discord, client, message) => {
@@ -6,31 +5,6 @@ module.exports = async (Discord, client, message) => {
 
     //checks if it is sent with a prefix or if it's sent by a bot
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    //creates a variable that contains the data of a profile of a user
-    //it checks whether the user who use interacted with the bot has a profile in the database already
-    //if it doesn't, it creates a new profile for the user
-    console.log("user sent a message!");
-    let profileData;
-    try {
-        console.log("attempting to create profile...");
-        profileData = await profileModel.findOne({ userID: message.author.id });
-        console.log("Checked if the user already has a profile");
-        if (!profileData) {
-            console.log("this user doesn't have a profile");
-            let profile = await profileModel.create({
-                userID: message.author.id,
-                serverID: message.guild.id,
-                coins: 1000,
-                bank: 0,
-            });
-            console.log("created new profile for this person");
-        } else {
-            console.log("this user already has a profile");
-        };
-    } catch(err) {
-        console.log(err);
-    }
 
     //creates a new variable called eventData
     let eventData;
